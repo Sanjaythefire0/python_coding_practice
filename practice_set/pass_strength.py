204d5492a5e76d0050836b82941ef10462ce406b
V Sanjay Kumar <124908241+Sanjaythefire0@users.noreply.github.com>
2026-04-09 14:21:35 +0530

    Delete practice_set directory

--- practice_set/pass_strength.py
+++
@@ -1,9 +0,0 @@
-def pass_strength(password):
-    if any(char.isdigit() for char in password) and any(char.isupper() for char in password) and len(password)>=8:
-        return "Strong"
-    else:
-        return "Weak"
-    
-if __name__=="__main__":
-    password=input("Enter password: ")
-    print(pass_strength(password))
